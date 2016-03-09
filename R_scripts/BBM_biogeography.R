a45e49b2c96cee8f3ec5d4c6eac53038c98566d0
James-Kitson <jameskitson10@gmail.com>
2016-03-09 14:00:16 +0000

    removed som libraries not used and renamed this so my script naming is consistent across the project.

--- R_scripts/BBM_biogeography.R
+++ R_scripts/BBM_biogeography.R
@@ -14,8 +14,6 @@ out<-paste(getwd(),"/Diagrams/",sep="")
 library(ape)
 library(phytools)
 library(plyr)
-library(RColorBrewer)
-library(BioGeoBEARS)
 
 ### read in the tree
 my.trees<-read.nexus("Data/All_dating_mcorrected.nex.con.tre")
