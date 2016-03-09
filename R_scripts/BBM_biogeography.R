51941766dc6ee4895c84453fd46465dfc4b48ef1
James-Kitson <jameskitson10@gmail.com>
2016-03-09 16:16:52 +0000

    Quite a few changes to plotting functions to make them format correctly in the rendered markdown document. I've also added a pdf dev so that this writes to a separate file but this isn't properly working yet (it adds the dev close messages to the rendered markdown).

--- R_scripts/BBM_biogeography.R
+++ R_scripts/BBM_biogeography.R
@@ -72,17 +72,13 @@ r.col<-c("#0000ff",
 ## @knitr BBMtreeplot
 
 ### plot the tree with the character reconstruction mapped
-#pdf(file=paste(out,"RASpBBM_biogeography.pdf",sep=""), 30, 30)
-plot(my.tree, show.node.label=FALSE, label.offset=0.0, cex=1)
-nodelabels(pie=Cratopus_anc, piecol=r.col, cex=0.5)
+plot(my.tree, show.node.label=FALSE, label.offset=0.0, cex=0.5)
+nodelabels(pie=Cratopus_anc, piecol=r.col, cex=0.75)
 ### add bayesian support values
-nodelabels(my.tree$node.label,adj=c(2,2),frame="none",
+nodelabels(my.tree$node.label,adj=c(1.5,1.5),frame="none",
             col=ifelse(my.tree$node.label>0.9,"red",
-                      ifelse(my.tree$node.label>=0.5 & my.tree$node.label<0.9,"blue","#0000ff00")),cex=1)
-### add node numbers
-my.tree$node.number<-seq(1, length(my.tree$node.label),1)
-nodelabels(my.tree$node.number,adj=c(3.2,3.2),frame="none",col="black")
-legend(x=0.005, y=25,
+                      ifelse(my.tree$node.label>=0.5 & my.tree$node.label<0.9,"blue","#0000ff00")),cex=0.5)
+legend(x=0.01, y=10,
        legend=c("Madagascar",
                 "Reunion",
                 "Mauritius",
@@ -97,7 +93,7 @@ legend(x=0.005, y=25,
                 "Seychelles",
                 "n/s"),
        fill=r.col,
-       cex=1)
+       cex=0.4)
 
 ### make an offset for the axis as R won't draw it from the tip to the root. The offset is a negative starting point for the axis equivalent to the
 ### round ing up we do at the root end of the axis i.e. if we round 4.79 Mya to 5 Mya then we need to offset by minus ~0.21Ma of distance measured in
@@ -111,4 +107,50 @@ axis(side=1,
      padj=1,
      at=seq(-offset, max(nodeHeights(my.tree)), by=(max(nodeHeights(my.tree))+offset)/(round_any(max(HPD$Median),0.5)/0.5)),
      labels=seq(round_any(max(HPD$Median),0.5),0,by=-0.5))
+
+## @ knitr dumpchunk
+######################################################################################################################################
+###################### Plot the tree to the output folder using a pdf dev #######################################
+######################################################################################################################################
+
+### plot the tree with the character reconstruction mapped
+#pdf(file=paste(out,"BBM_biogeography.pdf",sep=""), 30, 30)
+#plot(my.tree, show.node.label=FALSE, label.offset=0.0, cex=2)
+#nodelabels(pie=Cratopus_anc, piecol=r.col, cex=0.5)
+### add bayesian support values
+#nodelabels(my.tree$node.label,adj=c(2,2),frame="none",
+#           col=ifelse(my.tree$node.label>0.9,"red",
+#                      ifelse(my.tree$node.label>=0.5 & my.tree$node.label<0.9,"blue","#0000ff00")),cex=1)
+### add node numbers
+#my.tree$node.number<-seq(1, length(my.tree$node.label),1)
+#nodelabels(my.tree$node.number,adj=c(3.2,3.2),frame="none",col="black")
+#legend(x=0.005, y=25,
+#       legend=c("Madagascar",
+#                "Reunion",
+#                "Mauritius",
+#                "Rodrigues",
+#                "Europa",
+#                "Grande Glorieuse",
+#                "Moheli",
+#                "Anjouan",
+#                "Grande Comore",
+#                "Juan de Nova",
+#                "Aldabra",
+#                "Seychelles",
+#                "n/s"),
+#       fill=r.col,
+#       cex=1.5)
+
+### make an offset for the axis as R won't draw it from the tip to the root. The offset is a negative starting point for the axis equivalent to the
+### round ing up we do at the root end of the axis i.e. if we round 4.79 Mya to 5 Mya then we need to offset by minus ~0.21Ma of distance measured in
+### branch lengths. To do this we divide the root height by the root age and multiply by the difference between the oldest value on the axis and
+### the oldest value on the tree.
+#offset<-(round_any(max(HPD$Median),0.5)-max(HPD$Median))*(max(nodeHeights(my.tree))/max(HPD$Median))
+
+## put on a the correct axis
+#axis(side=1,
+#     cex.axis=1,
+#     padj=1,
+#     at=seq(-offset, max(nodeHeights(my.tree)), by=(max(nodeHeights(my.tree))+offset)/(round_any(max(HPD$Median),0.5)/0.5)),
+#     labels=seq(round_any(max(HPD$Median),0.5),0,by=-0.5))
 #dev.off()
