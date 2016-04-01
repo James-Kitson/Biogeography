3cb8b5919cb09605590922e00431641b0a7c6d22
James-Kitson <jameskitson10@gmail.com>
2016-04-01 18:11:01 +0100

    got rid of the bits dealing with resolving polytomies.

--- R_scripts/ML_biogeography_MCC.R
+++ R_scripts/ML_biogeography_MCC.R
@@ -14,7 +14,7 @@ library(plyr)
 
 ### read in the tree
 my.tree<-read.nexus("Data/trees/Cratopus_MCC_tree.nex")
-tree.resolve<-my.tree
+my.tree<-my.tree
 
 ### read in the node ages and heights from the vstat file from MrBayes - I have deleted all tip ages leaving only the root and internal nodes
 HPD<-read.csv("Data/bipartition_ages.csv")
@@ -29,24 +29,12 @@ name<-data.frame(lapply(name, as.character), stringsAsFactors=FALSE)
 ######################## Do some rough phylogeography ############################
 ########################################################################################################
 
-### resolve polytomies
-#tree.resolve.out<-multi2di(my.tree, random=TRUE)
-
-### write the tree to an ouptut file. The polytomies are resolved randomly so you need to write out a file and use that in future
-#write.nexus(tree.resolve.out, file = "Data/resolved_tree.nex")
-
-### read in the resolved tree
-#tree.resolve<-read.nexus("Data/resolved_tree.nex")
-
-### give zero length branches an arbitrarily short length
-#tree.resolve$edge.length<-ifelse(tree.resolve$edge.length==0,0.0000000001,tree.resolve$edge.length)
-
 ### read in the island data
 islands<-read.csv("Data/dist_ultra.csv", header=FALSE)
 colnames(islands)<-c("sample","island")
 
 ### calculate the ML character reconstruction
-Cratopus_anc<-ace(islands$island,tree.resolve,type="discrete")
+Cratopus_anc<-ace(islands$island,my.tree,type="discrete")
 
 ### set some colours for the islands
 r.col<-c("#0000ff",
@@ -64,22 +52,19 @@ r.col<-c("#0000ff",
          "#000000")
 
 ### Convert node lables to numeric and round to two dp
-tree.resolve$node.label<-as.numeric(tree.resolve$node.label)
-tree.resolve$node.label<-round(tree.resolve$node.label,digits=2)
+my.tree$node.label<-as.numeric(my.tree$node.label)
+my.tree$node.label<-round(my.tree$node.label,digits=2)
 
 ### the next line uses match to perform the same function as vlookup in excel
-tree.resolve$tip.label <- (name$Alt_label[match(tree.resolve$tip.label,name$Name)])
+my.tree$tip.label <- (name$Alt_label[match(my.tree$tip.label,name$Name)])
 
 ## @ knitr MLtreeplot
 
 ### plot the tree with the character reconstruction mapped
-plot(tree.resolve, show.node.label=FALSE, label.offset=0.0, cex=0.5)
+plot(my.tree, show.node.label=FALSE, label.offset=0.0, cex=0.5)
 ### add biogeographic piecharts
 nodelabels(pie = Cratopus_anc$lik.anc, piecol = r.col, cex = 0.75)
-### add bayesian support values
-nodelabels(tree.resolve$node.label,adj=c(1.5,1.5),frame="none",
-           col=ifelse(tree.resolve$node.label>0.9,"red",
-                      ifelse(tree.resolve$node.label>=0.5 & tree.resolve$node.label<0.9,"blue","#0000ff00")),cex=0.5)
+### make a legend
 legend(x=0.01, y=10,
        legend=c("Madagascar",
                 "Reunion",
@@ -101,13 +86,13 @@ legend(x=0.01, y=10,
 ### round ing up we do at the root end of the axis i.e. if we round 4.79 Mya to 5 Mya then we need to offset by minus ~0.21Ma of distance measured in
 ### branch lengths. To do this we divide the root height by the root age and multiply by the difference between the oldest value on the axis and
 ### the oldest value on the tree.
-offset<-(round_any(max(HPD$Median),0.5)-max(HPD$Median))*(max(nodeHeights(tree.resolve))/max(HPD$Median))
+offset<-(round_any(max(HPD$Median),0.5)-max(HPD$Median))*(max(nodeHeights(my.tree))/max(HPD$Median))
 
 ## put on a the correct axis
 axis(side=1,
      cex.axis=1,
      padj=1,
-     at=seq(-offset, max(nodeHeights(tree.resolve)), by=(max(nodeHeights(tree.resolve))+offset)/(round_any(max(HPD$Median),0.5)/0.5)),
+     at=seq(-offset, max(nodeHeights(my.tree)), by=(max(nodeHeights(my.tree))+offset)/(round_any(max(HPD$Median),0.5)/0.5)),
      labels=seq(round_any(max(HPD$Median),0.5),0,by=-0.5))
 
 ## @ knitr dumpchunk
@@ -117,17 +102,10 @@ axis(side=1,
 ######################################################################################################################################
 ### plot the tree with the character reconstruction mapped
 pdf(file="Diagrams/ML_biogeography_MCC.pdf", 30, 30)
-plot(tree.resolve, show.node.label=FALSE, label.offset=0.0, cex=2)
+plot(my.tree, show.node.label=FALSE, label.offset=0.0, cex=2)
 ### add biogeographic piecharts
 nodelabels(pie = Cratopus_anc$lik.anc, piecol = r.col, cex = 0.75)
-### add bayesian support values
-#nodelabels(tree.resolve$node.label,adj=c(2,2),frame="none",
-#           col=ifelse(tree.resolve$node.label>0.9,"red",
-#                      ifelse(tree.resolve$node.label>=0.5 & tree.resolve$node.label<0.9,"blue","#0000ff00")),cex=1)
-### add node numbers
-#tree.resolve$node.number<-seq(1, length(tree.resolve$node.label),1)
-nodelabels(tree.resolve$node.number,adj=c(3.2,3.2),frame="none",col="black")
-
+### add a legend
 legend(x=0.005, y=25,
        legend=c("Madagascar",
                 "Reunion",
@@ -149,13 +127,13 @@ legend(x=0.005, y=25,
 ### round ing up we do at the root end of the axis i.e. if we round 4.79 Mya to 5 Mya then we need to offset by minus ~0.21Ma of distance measured in
 ### branch lengths. To do this we divide the root height by the root age and multiply by the difference between the oldest value on the axis and
 ### the oldest value on the tree.
-offset<-(round_any(max(HPD$Median),0.5)-max(HPD$Median))*(max(nodeHeights(tree.resolve))/max(HPD$Median))
+offset<-(round_any(max(HPD$Median),0.5)-max(HPD$Median))*(max(nodeHeights(my.tree))/max(HPD$Median))
 
 ## put on a the correct axis
 axis(side=1,
      cex.axis=1,
      padj=1,
-     at=seq(-offset, max(nodeHeights(tree.resolve)), by=(max(nodeHeights(tree.resolve))+offset)/(round_any(max(HPD$Median),0.5)/0.5)),
+     at=seq(-offset, max(nodeHeights(my.tree)), by=(max(nodeHeights(my.tree))+offset)/(round_any(max(HPD$Median),0.5)/0.5)),
      labels=seq(round_any(max(HPD$Median),0.5),0,by=-0.5))
 
 dev.off()
