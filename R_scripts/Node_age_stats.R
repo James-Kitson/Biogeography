b620b080615f0aec0d07d7e4a22430dca1f1d07c
James-Kitson <jameskitson10@gmail.com>
2018-03-08 18:05:03 +0000

    forgot to upload the stats summary script

---
+++ R_scripts/Node_age_stats.R
@@ -0,0 +1,25 @@
+rm(list=ls())
+
+colonisation_means<-NULL
+
+for(i in 1:1000){
+  node_ages<-read.csv(paste("~/Desktop/Viper_holding/DEC/colonisation_means_unstratified_",i,".csv",sep=""))
+  colonisation_means<-rbind(colonisation_means,node_ages)
+}
+
+### test whether colonisations of Mauritius are significantly younger than colonisations of Reunion
+t.test(colonisation_means$Reu_Mau,colonisation_means$Mau_Reu, paired=TRUE)
+
+### test whether colonisation of Mauritius are younger than in situ speciation on Mauritius
+t.test(colonisation_means$Reu_Mau,colonisation_means$Mau_insitu, paired=TRUE)
+
+### test whether in situ speciation events on Reunion are significantly younger than colonisations of Reunion
+t.test(colonisation_means$Reu_insitu,colonisation_means$Mau_Reu, paired=TRUE)
+
+### test whether flightloss nodes are yonger than other nodes
+t.test(colonisation_means$fl_age,colonisation_means$not_fl_age, paired=TRUE)
+
+t.test(colonisation_means$Reu_Mau_count,colonisation_means$Mau_Reu_count, paired=TRUE)
+
+mean(colonisation_means$Reu_Mau_count)
+mean(colonisation_means$Mau_Reu_count)
