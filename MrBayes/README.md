a9c78a6afc8e0cb888c161fa937326b9dacc5a81
James-Kitson <jameskitson10@gmail.com>
2018-06-06 17:37:04 +0100

    First commit in the final steps of archiving this repo.

---
+++ MrBayes/README.md
@@ -0,0 +1,6 @@
+# Kitson et al Cratopine Biogeography - MrBayes
+
+This folder contains Input and output files for MrBayes single locus analyses. The files are as follows:
+
+- [Input files](https://github.com/James-Kitson/Biogeography/tree/master/MrBayes/Input_files)
+- [Tree files](https://github.com/James-Kitson/Biogeography/tree/master/MrBayes/Tree_files) - The locations of islands for calculating interisland distances.
--- R_scripts/Plot_MCC_ggtree.R
+++ R_scripts/Plot_MCC_ggtree.R
@@ -11,19 +11,20 @@ library(phytools)
 library(plyr)
 library(ggtree)
 
-names<-read.csv("Metadata/Multilocus_names_final.csv", stringsAsFactors = FALSE)
-clades<-read.csv("Metadata/Multilocus_clades.csv", stringsAsFactors = FALSE)
+names<-read.csv("StarBEAST/Multilocus_names_starBEAST.csv", stringsAsFactors = FALSE)
+clades<-read.csv("StarBEAST/Multilocus_clades.csv", stringsAsFactors = FALSE)
 
 #############################################################################
 #### check MrBayes vs BEAST topology
 #############################################################################
 
-BEASTMCC <- read.beast(file = "BEAST/Tree_and_log_files/Relaxed_clock_modeltest2_Good_age/Relaxed_clock_MCC.tree")
+#BEASTMCC <- read.beast(file = "BEAST/Tree_and_log_files/Relaxed_clock_modeltest2_Good_age/Relaxed_clock_MCC.tree")
+BEASTMCC <- read.beast(file = "StarBEAST//starBEAST_MCC.tree")
 
 ### spit out a newick tree to use with BioGeoBEARS for the relaxed BEAST analysis
-write.tree(BEASTMCC@phylo, file = "BEAST/Tree_and_log_files/Relaxed_clock_modeltest2_Good_age/Relaxed_clock_MCC_newick.tree")
+#write.tree(BEASTMCC@phylo, file = "BEAST/Tree_and_log_files/Relaxed_clock_modeltest2_Good_age/Relaxed_clock_MCC_newick.tree")
 
-BEASTMCC@phylo$tip.label<-names$Alt_label[match(BEASTMCC@phylo$tip.label, names$Name)]
+#BEASTMCC@phylo$tip.label<-names$Alt_label[match(BEASTMCC@phylo$tip.label, names$starBEAST_label)]
 
 #############################################################################
 ### Plot the BEAST tree with different annotations and node bars 
@@ -36,6 +37,7 @@ BEASTMCC_plot_support<-ggtree(BEASTMCC, ndigits=2) +
   geom_text2(aes(label=posterior, subset=posterior<0.95), vjust=-0.1,  color='blue') +
   ggtitle("Support Values") +
   #ggplot2::xlim(0, 25) +
+  geom_tiplab() +
   theme_tree2()
 BEASTMCC_plot_support<-BEASTMCC_plot_support %<+% clades + geom_tiplab(aes(colour= colour))
 
@@ -76,11 +78,19 @@ BEASTMCC_plot_nodeageHPD<-ggtree(BEASTMCC, ndigits=2) +
 BEASTMCC_plot_nodeageHPD<-BEASTMCC_plot_nodeageHPD %<+% clades + geom_tiplab(aes(colour= colour))
 
 
-pdf(file = "Figures/BEAST_Relaxed_clock_MCC.pdf", 8.27,11.69)
-revts(rotate(BEASTMCC_plot_support,59)) + ggplot2::xlim(-15, 20)
-revts(rotate(BEASTMCC_plot_nodes,59)) + ggplot2::xlim(-15, 20)
-revts(rotate(BEASTMCC_plot_nodeage,59)) + ggplot2::xlim(-15, 20)
-revts(rotate(BEASTMCC_plot_nodebars,59)) + ggplot2::xlim(-15, 20)
-revts(rotate(BEASTMCC_plot_nodeageHPD,59)) + ggplot2::xlim(-15, 20)
-dev.off()
+#pdf(file = "Figures/BEAST_Relaxed_clock_MCC.pdf", 8.27,11.69)
+#revts(rotate(BEASTMCC_plot_support,59)) + ggplot2::xlim(-15, 20)
+#revts(rotate(BEASTMCC_plot_nodes,59)) + ggplot2::xlim(-15, 20)
+#revts(rotate(BEASTMCC_plot_nodeage,59)) + ggplot2::xlim(-15, 20)
+#revts(rotate(BEASTMCC_plot_nodebars,59)) + ggplot2::xlim(-15, 20)
+#revts(rotate(BEASTMCC_plot_nodeageHPD,59)) + ggplot2::xlim(-15, 20)
+#dev.off()
 
+
+pdf(file = "StarBEAST/BEAST_Relaxed_clock_MCC.pdf", 8.27,11.69)
+revts(rotate(BEASTMCC_plot_support,59)) + ggplot2::xlim(-30, 20)
+revts(rotate(BEASTMCC_plot_nodes,59)) + ggplot2::xlim(-30, 20)
+revts(rotate(BEASTMCC_plot_nodeage,59)) + ggplot2::xlim(-30, 20)
+revts(rotate(BEASTMCC_plot_nodebars,59)) + ggplot2::xlim(-30, 20)
+revts(rotate(BEASTMCC_plot_nodeageHPD,59)) + ggplot2::xlim(-30, 20)
+dev.off()
